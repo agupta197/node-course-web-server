011830d8ffe931bbc546fa1efabecaed329c2bcf
Anurag Gupta <agupta197@sapient.com>
2018-06-01 10:17:15 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,54 @@
+
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFileSync('server.log', log + '\n', err => {
+        console.log('Unable to write log');
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+
+hbs.registerHelper('screamIt', (text) => text.toUpperCase());
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website'
+    });
+});
+
+app.get('/about', (req, res) => {
+    // res.send('About Page');
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Bad Request'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000!');
+});
